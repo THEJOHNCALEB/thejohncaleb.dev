8e86b06885c1d84947d2e8464c5d212d4b2dbfbc
John Caleb <Calebjohn3112@gmail.com>
2026-05-09 10:51:05 +0100

    added cv redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import mdx from "@astrojs/mdx";
 // TODO: remove plugins
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thejohncaleb.vercel.app",
+  site: "https://thejohncaleb.com",
   integrations: [tailwind(), icon(), solidJs(), mdx()],
   markdown: {
     syntaxHighlight: "prism",
@@ -17,5 +17,6 @@ export default defineConfig({
   },
   redirects: {
     "/live": { status: 302, destination: "https://youtube.com/@thejohncaleb" },
+    "/cv": { status: 302, destination: "https://drive.google.com/file/d/1i-tXJGW5Sp4w28y5QKlxz6x3jLsuppbQ/view" },
   },
 });
